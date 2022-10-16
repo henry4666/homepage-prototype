9a234fd435fa4af174890107f438a3b11f30dca8
henry4666 <henrilac22@gmail.com>
2022-10-16 16:40:19 -0300

    create typewriter loop

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const into = document.querySelector("#into");
+"use strict"
+
+const typed = document.querySelector("#typed");
+
+function deleteTextAnimation(txt) {
+  txt.classList.replace("in", "out");
+};
+
+function typeTextAnimation(txt) {  
+  txt.classList.replace("out", "in");
+};
+
+// setTimeout(typing(), 6000);
