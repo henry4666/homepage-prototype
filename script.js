a81df774895261c74bd1ea612ec2b21b4b2e9ab2
henry4666 <henrilac22@gmail.com>
2022-10-16 19:46:15 -0300

    implement title text shift

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const seconds = 1000;  // 1 second
 let initDuration = 3*seconds;
 const animationDelay = 1.5*seconds;
 const typed = document.querySelector("#typed");
+const titleText = ["Engineer.", "Designer.", "Developer."];
 
 function deleteTextAnimation(txt) {
   txt.classList.replace("in", "out");
@@ -13,12 +14,18 @@ function typeTextAnimation(txt) {
   txt.classList.replace("out", "in");
 };
 
+function textShift(newTxt) {
+  typed.innerHTML = newTxt;
+}
+
+textShift(titleText[0]);
+
 function animationLoop() {
   for (let i=1 ; i<=2 ; i++) {
     initDuration *= i;
 
     setTimeout( ()=> {deleteTextAnimation(typed)}, initDuration);
-    setTimeout( ()=> {console.log("text change")}, initDuration + animationDelay);  // text replace and color change function
+    setTimeout( ()=> {textShift(titleText[i])}, initDuration + animationDelay);
     setTimeout( ()=> {typeTextAnimation(typed)}, initDuration + animationDelay + 0.1*seconds);
 
     initDuration += animationDelay;
