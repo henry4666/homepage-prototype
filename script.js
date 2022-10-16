3dee05ae81c1f9d8a3440c9be445289290019ac1
henry4666 <henrilac22@gmail.com>
2022-10-16 18:55:36 -0300

    create animation loop function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const seconds = 1000;  // 1 second
+let initDuration = 3*seconds;
 const typed = document.querySelector("#typed");
 
 function deleteTextAnimation(txt) {
@@ -10,12 +12,18 @@ function typeTextAnimation(txt) {
   txt.classList.replace("out", "in");
 };
 
-function fullAnimation(txt) {
-  const seconds = 1000;  // 1 second
-  let initDuration = 3*seconds;
+function animationLoop() {
+  for (let i=1 ; i<2 ; i++) {
+    initDuration *= i
 
+    setTimeout( ()=> {deleteTextAnimation(typed)}, initDuration);
+    setTimeout( ()=> {console.log("text change")}, initDuration + 1.5*seconds);  // text replace and color change function
+    setTimeout( ()=> {typeTextAnimation(typed)}, initDuration + 1.6*seconds);
+  };
+};
 
-  setTimeout(deleteTextAnimation(txt), initDuration);
-  setTimeout("", initDuration + 1.5*seconds);  // text replace and color change function
-  setTimeout(typeTextAnimation(txt), initDuration + 1.6*seconds);
-}
+function fullAnimationLoop(loops) {
+  for () {
+    animationLoop();
+  };
+};
