bfe0752c131ba4a8321d3736afb6338123dd7cab
henry4666 <henrilac22@gmail.com>
2022-10-16 20:44:30 -0300

    add color looping to function

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ const seconds = 1000;  // 1 second
 let initDuration = 3*seconds;
 const animationDelay = 1.5*seconds;
 const typed = document.querySelector("#typed");
+const shape = document.querySelector("#shape");
 const titleText = ["Engineer.", "Designer.", "Developer."];
+const loopCSSColors = ["loop-color1", "loop-color2", "loop-color3"];
 
 function deleteTextAnimation(txt) {
   txt.classList.replace("in", "out");
 };
 
-function typeTextAnimation(txt) {  
+function typeTextAnimation(txt) {
   txt.classList.replace("out", "in");
+};
+
+function colorShift(txt, circle, color, i) {
+  txt.classList.replace(color[i-1], color[i]);
+  circle.classList.replace(color[i-1], color[i]);
 };
 
 function textShift(newTxt) {
@@ -25,13 +32,18 @@ function animationLoop() {
     initDuration *= i;
 
     setTimeout( ()=> {deleteTextAnimation(typed)}, initDuration);
-    setTimeout( ()=> {textShift(titleText[i])}, initDuration + animationDelay);
+
+    setTimeout( ()=> {
+      textShift(titleText[i]);
+      colorShift(typed, shape, loopCSSColors, i);
+    }, initDuration + animationDelay);
+
     setTimeout( ()=> {typeTextAnimation(typed)}, initDuration + animationDelay + 0.1*seconds);
 
     initDuration += animationDelay;
   };
 };
-``
+
 function fullAnimationLoop(loops) {
     animationLoop();
 };
