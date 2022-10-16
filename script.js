2a1fc54c8834d41e07bbf34f93c1d7eb0a28253a
henry4666 <henrilac22@gmail.com>
2022-10-16 18:39:01 -0300

    add fullAnimation function

--- script.js
+++ script.js
@@ -10,4 +10,12 @@ function typeTextAnimation(txt) {
   txt.classList.replace("out", "in");
 };
 
-function fullAnimation(txt) {}
+function fullAnimation(txt) {
+  const seconds = 1000;  // 1 second
+  let initDuration = 3*seconds;
+
+
+  setTimeout(deleteTextAnimation(txt), initDuration);
+  setTimeout("", initDuration + 1.5*seconds);  // text replace and color change function
+  setTimeout(typeTextAnimation(txt), initDuration + 1.6*seconds);
+}
